003e0b34956fdb2929d373b456ab7dfb56ee8225
rohnit110 <130657907+rohnit110@users.noreply.github.com>
2024-03-20 17:34:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Weather1 from './component/weatherApp/Weather1';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Weather1/>
+    </div>
+  );
+}
+
+export default App;
